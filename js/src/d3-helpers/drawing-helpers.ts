4c81e60e5d7328cde725a4993fdedffdf4028653
koreanwglasses <koreanwglasses@gmail.com>
2022-07-06 13:40:50 -0500

    Fixed transitions

--- js/src/d3-helpers/drawing-helpers.ts
+++ js/src/d3-helpers/drawing-helpers.ts
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import { NumberValue } from "d3";
 
-type Selection = d3.Selection<any, any, any, any>;
+type Selection<D = any> = d3.Selection<any, D, any, any>;
 type AttributeValue<D> =
   | string
   | number
@@ -44,18 +44,34 @@ export const helpers = ({
       y: d3.ScaleLinear<number, number>,
       color: AttributeValue<{ x: X; y: Y }> = d3["schemeCategory10"][0]
     ) =>
-    <S extends Selection>(g: S) =>
-      g
-        .selectAll("rect")
+    <S extends Selection>(g: S) => {
+      const rects = g.selectAll("rect");
+
+      let prevData = rects.data() as { x: X; y: Y }[];
+      if (!Array.isArray(prevData)) prevData = [];
+      const prevX = new Set(prevData.map((d) => d.x));
+
+      const applyAttributes = (g: any) =>
+        (g as Selection<{ x: X; y: Y }>)
+          .attr("x", (d) => x(d.x)!)
+          .attr("width", x.bandwidth())
+          .attr("y", (d) => y(d.y))
+          .attr("height", (d) => height - y(d.y))
+          .attr("fill", color as any);
+
+      return rects
         .data(data)
         .join("rect")
         .call((g) =>
+          // Transition on previously existing categories
           g
+            .filter((d) => prevX.has(d.x))
             .transition()
-            .attr("x", (d) => x(d.x)!)
-            .attr("width", x.bandwidth())
-            .attr("y", (d) => y(d.y))
-            .attr("height", (d) => height - y(d.y))
-            .attr("fill", color as any)
-        ),
+            .call(applyAttributes)
+        )
+        .call((g) =>
+          // Instantly apply affects to new categories
+          g.filter((d) => !prevX.has(d.x)).call(applyAttributes)
+        );
+    },
 });
