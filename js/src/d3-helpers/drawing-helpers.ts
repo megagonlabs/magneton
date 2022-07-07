1da6f22d3b137fc417d7ebf9aa7a6557909d4055
koreanwglasses <koreanwglasses@gmail.com>
2022-07-06 19:04:57 -0500

    Simplified transition code

--- js/src/d3-helpers/drawing-helpers.ts
+++ js/src/d3-helpers/drawing-helpers.ts
@@ -2,6 +2,7 @@ import * as d3 from "d3";
 import { NumberValue } from "d3";
 
 type Selection<D = any> = d3.Selection<any, D, any, any>;
+
 type AttributeValue<D> =
   | string
   | number
@@ -19,61 +20,50 @@ export const helpers = ({
 }: {
   width: number;
   height: number;
-}) => ({
-  xAxis:
-    <A extends d3.AxisScale<any>>(x: A) =>
-    <S extends Selection>(g: S) =>
-      g
-        .call((g) =>
-          g.transition().attr("transform", "translate(0," + height + ")")
-        )
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)"),
+}) => {
+  return {
+    xAxis:
+      <A extends d3.AxisScale<any>>(x: A) =>
+      <S extends Selection>(g: S) =>
+        g
+          .call((g) =>
+            g.transition().attr("transform", "translate(0," + height + ")")
+          )
+          .call(d3.axisBottom(x))
+          .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-65)"),
 
-  yAxis:
-    <A extends d3.AxisScale<any>>(y: A) =>
-    <S extends Selection>(g: S) =>
-      g.transition().call(d3.axisLeft(y)),
+    yAxis:
+      <A extends d3.AxisScale<any>>(y: A) =>
+      <S extends Selection>(g: S) =>
+        g.transition().call(d3.axisLeft(y)),
 
-  bars:
-    <X, Y extends NumberValue>(
-      data: { x: X; y: Y }[],
-      x: d3.ScaleBand<X>,
-      y: d3.ScaleLinear<number, number>,
-      color: AttributeValue<{ x: X; y: Y }> = d3["schemeCategory10"][0]
-    ) =>
-    <S extends Selection>(g: S) => {
-      const applyAttributes = (g: any) =>
-        (g as Selection<{ x: X; y: Y }>)
-          .attr("x", (d) => x(d.x)!)
-          .attr("width", x.bandwidth())
-          .attr("y", (d) => y(d.y))
-          .attr("height", (d) => height - y(d.y))
-          .attr("fill", color as any);
-
-      const rects = g.selectAll("rect");
-
-      let prevData = rects.data() as { x: X; y: Y }[];
-      if (!Array.isArray(prevData)) prevData = [];
-      const prevX = new Set(prevData.map((d) => d.x));
-
-      return rects
-        .data(data)
-        .join("rect")
-        .call((g) =>
-          // Transition only on previously existing categories
+    bars:
+      <X, Y extends NumberValue>(
+        data: { x: X; y: Y }[],
+        x: d3.ScaleBand<X>,
+        y: d3.ScaleLinear<number, number>,
+        color: AttributeValue<{ x: X; y: Y }> = d3["schemeCategory10"][0]
+      ) =>
+      <S extends Selection>(g: S) => {
+        const attr = (g: Selection<{ x: X; y: Y }>) =>
           g
-            .filter((d) => prevX.has(d.x))
-            .transition()
-            .call(applyAttributes)
-        )
-        .call((g) =>
-          // Instantly apply affects to new categories
-          g.filter((d) => !prevX.has(d.x)).call(applyAttributes)
-        );
-    },
-});
+            .attr("x", (d) => x(d.x)!)
+            .attr("width", x.bandwidth())
+            .attr("y", (d) => y(d.y))
+            .attr("height", (d) => height - y(d.y))
+            .attr("fill", color as any);
+
+        return g
+          .selectAll("rect")
+          .data(data)
+          .join(
+            (enter) => enter.append("rect").call(attr),
+            (update) => update.transition().call(attr as any)
+          );
+      },
+  };
+};
