53d135f516a2c45e09d7f6e4e6fe0d6f964c43da
koreanwglasses <koreanwglasses@gmail.com>
2022-07-08 12:16:07 -0500

    Added GraphData type

--- js/src/types/data-types.ts
+++ js/src/types/data-types.ts
@@ -1,4 +1,31 @@
 export type CategoricalDatum = { x: string; y: number };
 
 /** @deprecated */
-export type CategoricalData = CategoricalDatum[]
+export type CategoricalData = CategoricalDatum[];
+
+export type NodeData = {
+  id: string;
+  index: number;
+  x: number;
+  y: number;
+  vy: number;
+  vx: number;
+};
+
+export type LinkData = {
+  label: string;
+  source: Node;
+  target: Node;
+  weight: number;
+  sibling_index: number;
+  index: number;
+};
+
+export type GraphData = {
+  node_radius: number;
+  link_distance: number;
+  collision_scale: number;
+  link_width_scale: number;
+  graph_json_links: LinkData[];
+  graph_json_nodes: NodeData[];
+};
